1bc5d069836407f090fccdcbbbd0febc3ea62c5d
안수아 <suea724@gmail.com>
2016-03-28 13:56:05 +0900

    12
    12

---
+++ w4main.py
@@ -0,0 +1,23 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+nBigger=2
+for i in range(0,10):
+    if(i%nBigger):
+        print i
+def makeSwirlSquare(size):
+    sides=10
+    bigger=10
+    angle=90
+    t1.home()
+    t1.clear()
+    for i in range(0,sides):
+        if(i%2):
+            
+            size=size+bigger
+            #size+=bigger
+            t1.fd(size)
+            t1.right(angle)
+makeSwirlSquare(10)  
+
+wn.exitonclick()
